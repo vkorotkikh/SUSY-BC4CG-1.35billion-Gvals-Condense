a9cf72f280094e7e71ab7d921f4fa5646e3fa192
vkorotkikh <va.korotki@gmail.com>
2017-04-18 08:59:27 -0400

    Bugchecked version for elle - alpha mat results

--- vierergruppe_mats.py
+++ vierergruppe_mats.py
@@ -31,15 +31,10 @@ def main():
 # Performing the coefficient calculation
 def elle_class_calc():
 
-
 	vflops		= vierergruppe_flops()
 	# elle_bin	= flip_ellebin()
-	# for i in vflops:
-
 	for vset in vflops:
 		# print(vset[0], "V set")
-
-		# temp_flips	= flip_ellebin(vset[0])
 		print("Vierergruppe:", vset[0],"V")
 		print("Flop Ops:", vset[1])
 		print("")
@@ -49,19 +44,50 @@ def elle_class_calc():
 		for i in range(0, 6):
 			temp_pie	= cis_seed_pies(i)
 			# print("Pie i:", i)
-			""" temp_flips contains a Group of flips associated with a Vierergruppe
-			so 8 sets of 4. """
-			temp_flips	= flip_ellebin(vset[0])
-			print("Flips:",temp_flips)
-			print("")
+
 			""" For i Adinkra out of selected Pie slice	"""
 			for itet, adinkra in enumerate(temp_pie):
+				""" temp_flips contains a Group of flips associated with a Vierergruppe
+				so 8 sets of 4. """
+				temp_flips	= flip_ellebin(vset[0])
+				print("Flips:",temp_flips)
+				print("")
+				print("Pie #", i, "Adinkra", itet)
+				for flip_ops in temp_flips:
+					print("Current Flip:", flip_ops)
+					print("")
+					vijres_temp	= []
+					newrep_temp = []
+					for x in range(0, len(vset[1])):
+						flop_tup 	=	vset[1][x]
+						flop_op		=	vset[1][x][1]
+						assoc_flip	=	flip_ops[x]
+						# print("Current Flop:", flop_op, "	Flip:", flip_ops[x], binaries(flip_ops[x]))
+						temp_flop		=	colorspace_flop(adinkra, flop_op)
+						temp_flip		=	colorspace_flip(temp_flop, binaries(flip_ops[x]))
+						vijset, newrep	=	vij_holoraumy_4x4.calculate_vijmatset_nicely(temp_flip)
+						if vijset not in vijres_temp:
+							vijres_temp.append(vijset)
+							if len(vijres_temp) > 1:
+								print("Current Flop:", flop_op, "	Flip:", flip_ops[x])
+						else:
+							pass
 
+						if newrep not in newrep_temp:
+							newrep_temp.append(newrep)
+						else:
+							pass
+					print("vijres_temp")
+					for zz in range(0, len(vijres_temp)):
+						print(vijres_temp[zz])
+					print("newrep_temp")
+					for zz in range(0, len(newrep_temp)):
+						print(newrep_temp[zz])
+					print("		")
 				# for x in range(0, len(vset[1])):
 				# 	flop_tup 	= vset[1][x]
 				# 	flop_op		= vset[1][x][1]
 				# 	# print("vset[1]:", flop_tup)
-				#
 				# 	for flip_ops in temp_flips:
 				# 		assoc_flip	=	flip_ops[x]
 				# 		print("")
@@ -71,18 +97,6 @@ def elle_class_calc():
 				# 		temp_flip	=	colorspace_flip(temp_flop, binaries(flip_ops[x]))
 				# 		vijset		=	vij_holoraumy_4x4.calculate_vijmatset(temp_flip)
 				# 		print(vijset)
-				for flip_ops in temp_flips:
-					for x in range(0, len(vset[1])):
-						flop_tup 	=	vset[1][x]
-						flop_op		=	vset[1][x][1]
-						assoc_flip	=	flip_ops[x]
-						print("")
-						print("Pie i", i, "Adinkra", itet)
-						print("Current Flop:", flop_op, "	Flip:", flip_ops[x], binaries(flip_ops[x]))
-						temp_flop		=	colorspace_flop(adinkra, flop_op)
-						temp_flip		=	colorspace_flip(temp_flop, binaries(flip_ops[x]))
-						vijset, newrep	=	vij_holoraumy_4x4.calculate_vijmatset_nicely(temp_flip)
-						print(vijset)
 	# for vset, flop_ops in vflops.items():
 	# 	print(vset,"V")
 	# 	vset_flips	= elle_bin[vset]
@@ -112,19 +126,26 @@ def vierergruppe_flops():
 					("13)(24)", [3,4,1,2]), ("(14)(23)", [4,3,2,1])]
 
 	vgrp12v		= [ ("(12)", [2,1,3,4]), ("(34)", [1,2,4,3]),
-					("(1324)", [3,4,2,1]), ("(1423)", [4,3,1,2]) ]
+					# ("(1324)", [3,4,2,1]), ("(1423)", [4,3,1,2]) ]
+					("(1324)", [4,3,1,2]), ("(1423)", [3,4,2,1]) ]
 
-	vgrp13v		= [ ("(13)", [3,2,1,4]), ("(1234)", [2,3,4,1]),
-					("(24)", [1,4,3,2]), ("(1432)", [4,1,2,3])	]
+	vgrp13v		= [ ("(13)", [3,2,1,4]), ("(1234)", [4,1,2,3]),
+					("(24)", [1,4,3,2]), ("(1432)", [2,3,4,1])	]
 
-	# vgruppe	= 	{
-	# 			'()': vgrpv,
-	# 			'(12)': vgrp12v,
-	# 			'(13)': vgrp13v
-	# 			}
+	vgrp23v		= [ ("(23)", [1,3,2,4]), ("(1342)", [2,4,1,3]),
+					("(1243)", [3,1,4,2]), ("(14)", [4,2,3,1])	]
 
+	vgrp123v	= [ ("(132)", [3,1,2,4]), ("(132)", [4,2,1,3]),
+					("(243)", [1,3,4,2]), ("(142)", [2,4,3,1])	]
+
+	vgrp132v	= [ ("(132)", [2,3,1,4]), ("(234)", [1,4,2,3]),
+					("(124)", [4,1,3,2]), ("(143)", [3,2,4,1])	]
+
+	# vgrp13v		= [ ("(13)", [3,2,1,4]), ("(1234)", [2,3,4,1]),
+	# 				("(24)", [1,4,3,2]), ("(1432)", [4,1,2,3])	]
 	# vgruppe 	= [ ('()', vgrpv), ('(12)',vgrp12v), ('(13)', vgrp13v) ]
-	vgruppe 	= [ ('()', vgrpv), ('(12)',vgrp12v)]
+	# vgruppe 	= [ ('()', vgrpv), ('(12)',vgrp12v)]
+	vgruppe	   = [ ('(23)', vgrp23v), ('(123)', vgrp123v), ('(132)', vgrp132v) ]
 
 	return vgruppe
 
@@ -257,7 +278,8 @@ def pieslices(pie_index):
 # ********************************
 # Perform flop operation over Adinkra color space
 def colorspace_flop(adinkra, flop_op):
-	# print("Flop Operation", flop_op)
+	# print("	")
+	# print("Executing colorspace_flip", flop_op)
 	# print("Adinkra")
 	# print(adinkra)
 	# print("")
@@ -270,24 +292,22 @@ def colorspace_flop(adinkra, flop_op):
 # Perform flip operation over Adinkra color space
 def colorspace_flip(adinkra, flip_op):
 
-	print("Flip Operation", flip_op)
-	new_adinkra	= []
-
+	print("Executing colorspace_flip", flip_op)
 	""" Weird bug here if you do the algorith this way """
 	# new_adinkra	= []
-
 	# for i in range(0, len(adinkra)):
 	# 	tmat 		= adinkra[i]
 	# 	tmat[1:]	= tmat[1:] * flip_op[i]
 	# 	new_adinkra.append(tmat)
+	""" Normal algorithm """
+	new_adinkra	= []
 	for i in range(0, len(flip_op)):
 		# new_adinkra[i][1:]	= new_adinkra[i][1:] * flip_op[i]
 		temp_mat	= adinkra[i] * flip_op[i]
 		new_adinkra.append(temp_mat)
-
-	print("Flipped Adinkra")
-	print(new_adinkra)
-	print("")
+	# print("Flipped Adinkra")
+	# print(new_adinkra)
+	# print("")
 	return new_adinkra
 
 
